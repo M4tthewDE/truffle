3343f77b7ad26fe113e6f4fcc9b70cbaf8c3fceb
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-27 22:42:22 +0100

    Move twitch auth endpoints to twitch package

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,10 +1,6 @@
 package handlers
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
@@ -12,6 +8,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/m4tthewde/truffle/internal/config"
 	"github.com/m4tthewde/truffle/internal/session"
+	"github.com/m4tthewde/truffle/internal/twitch"
 )
 
 type UserInfo struct {
@@ -33,14 +30,14 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	login, err := getToken(params.Get("code"))
+	login, err := twitch.GetToken(params.Get("code"), config.Conf.ClientId, config.Conf.ClientSecret)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	validation, err := validateToken(login.AccessToken)
+	validation, err := twitch.ValidateToken(login.AccessToken)
 	if err != nil {
 		log.Println(err)
 		w.WriteHeader(http.StatusBadRequest)
@@ -51,80 +48,4 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	s := session.NewSession(sessionId, login.AccessToken, validation.Login, validation.UserId)
 	session.AddSession(s)
 	http.Redirect(w, r, "http://localhost:8080/#"+sessionId.String(), http.StatusFound)
-}
-
-type TokenResponse struct {
-	AccessToken string `json:"access_token"`
-}
-
-func getToken(code string) (*TokenResponse, error) {
-	body := make(map[string]string)
-	body["client_id"] = config.Conf.ClientId
-	body["client_secret"] = config.Conf.ClientSecret
-	body["code"] = code
-	body["grant_type"] = "authorization_code"
-	body["redirect_uri"] = "http://localhost:8080/login"
-
-	jsonStr, err := json.Marshal(body)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", "https://id.twitch.tv/oauth2/token", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, errors.New(resp.Status)
-	}
-
-	var loginResponse TokenResponse
-	err = json.NewDecoder(resp.Body).Decode(&loginResponse)
-	if err != nil {
-		return nil, err
-	}
-
-	return &loginResponse, nil
-}
-
-type ValidationResponse struct {
-	UserId string `json:"user_id"`
-	Login  string `json:"login"`
-}
-
-func validateToken(accessToken string) (*ValidationResponse, error) {
-	req, err := http.NewRequest("GET", "https://id.twitch.tv/oauth2/validate", nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Authorization", fmt.Sprintf("OAuth %s", accessToken))
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, errors.New(resp.Status)
-	}
-
-	var userInfo ValidationResponse
-	err = json.NewDecoder(resp.Body).Decode(&userInfo)
-	if err != nil {
-		return nil, err
-	}
-
-	return &userInfo, nil
 }
--- internal/twitch/twitch.go
+++ internal/twitch/twitch.go
@@ -197,3 +197,79 @@ func GetChannelId(authentication Authentication, channel string) (string, error)
 
 	return channelResponse.Data[0].Id, nil
 }
+
+type TokenResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+func GetToken(code string, clientId string, clientSecret string) (*TokenResponse, error) {
+	body := make(map[string]string)
+	body["client_id"] = clientId
+	body["client_secret"] = clientSecret
+	body["code"] = code
+	body["grant_type"] = "authorization_code"
+	body["redirect_uri"] = "http://localhost:8080/login"
+
+	jsonStr, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", "https://id.twitch.tv/oauth2/token", bytes.NewBuffer(jsonStr))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
+	var loginResponse TokenResponse
+	err = json.NewDecoder(resp.Body).Decode(&loginResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return &loginResponse, nil
+}
+
+type ValidationResponse struct {
+	UserId string `json:"user_id"`
+	Login  string `json:"login"`
+}
+
+func ValidateToken(accessToken string) (*ValidationResponse, error) {
+	req, err := http.NewRequest("GET", "https://id.twitch.tv/oauth2/validate", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("OAuth %s", accessToken))
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
+	var userInfo ValidationResponse
+	err = json.NewDecoder(resp.Body).Decode(&userInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &userInfo, nil
+}
