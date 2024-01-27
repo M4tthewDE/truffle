d8ed1f7c189ae35fe4b0400b6640cd99473d1f35
M4tthewDE <matthias.kronberg@gmail.com>
2024-01-27 12:06:24 +0100

    Add util for session extraction

--- internal/root.go
+++ internal/root.go
@@ -1,13 +1,9 @@
 package internal
 
 import (
-	"errors"
 	"log"
 	"net/http"
-	"strings"
 	"text/template"
-
-	"github.com/google/uuid"
 )
 
 type RootHandler struct {
@@ -45,23 +41,4 @@ func (handler *RootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-}
-
-func sessionIdFromRequest(r *http.Request) (*uuid.UUID, error) {
-	sessionCookie, err := r.Cookie("sessionid")
-	if err != nil {
-		return nil, err
-	}
-
-	splits := strings.Split(sessionCookie.String(), "=")
-	if len(splits) < 2 {
-		return nil, errors.New("invalid cookie format")
-	}
-
-	sessionId, err := uuid.Parse(splits[1])
-	if err != nil {
-		return nil, err
-	}
-
-	return &sessionId, nil
 }
---
+++ internal/util.go
@@ -0,0 +1,28 @@
+package internal
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+func sessionIdFromRequest(r *http.Request) (*uuid.UUID, error) {
+	sessionCookie, err := r.Cookie("sessionid")
+	if err != nil {
+		return nil, err
+	}
+
+	splits := strings.Split(sessionCookie.String(), "=")
+	if len(splits) < 2 {
+		return nil, errors.New("invalid cookie format")
+	}
+
+	sessionId, err := uuid.Parse(splits[1])
+	if err != nil {
+		return nil, err
+	}
+
+	return &sessionId, nil
+}
